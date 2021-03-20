54ba3917f23acca8d6b5985a4c0a3dfafa7a219f
Mary Bellavia <sauerwein.mary@gmail.com>
2021-03-20 11:52:29 -0500

    attempting to connect sqllite database

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from flask import (
     jsonify,
     request,
     redirect)
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+import pandas, sqlite3, csv
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String, Float
 
 # flask setup
 app = Flask(__name__)
 
-from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
@@ -19,15 +25,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-Rent = create_rent(db)
-House = create_house(db)
+# Establish Connection
+engine = create_engine("sqlite:///rent_house.sqlite")
+conn = engine.connect()
+
+house_df = pandas.read_csv("static/data/house_cleaned.csv")
+house_df.to_sql("House", conn, if_exists='append', index=False)
+
+rent_df = pandas.read_csv("static/data/rent_cleaned.csv")
+rent_df.to_sql("Rent", conn, if_exists='append', index=False)
+
+ # Create both the Surfer and Board tables within the database
+Base.metadata.create_all(conn)
+
+from sqlalchemy.orm import Session
+session = Session(bind=engine)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
     # Return template and data
-    return render_template("index.html", title="Rent & House Index Analysis")
-
+    return render_template("index.html", title="Rent & House Index Analysis", )
 @app.route("/about")
 def about():
     # Return template and data
@@ -37,8 +55,9 @@ def about():
 @app.route("/models")
 def models():
 
+    rent_list = session.query(Rent)
     # Redirect back to home page
-    return render_template("models.html", title="Models")
+    return render_template("models.html", title="Models", rent=rent_list)
 
 @app.route("/maps")
 def maps():

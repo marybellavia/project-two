d723f19090712cd2ff526fb35079f0d77891fd6e
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-03-23 18:48:40 -0500

    adding mockup for what I want my model to look like

--- static/js/deblois.js
+++ static/js/deblois.js
@@ -3,18 +3,27 @@ function GetCityName(){
     document.getElementById('citySearchBox').value = "";
 }
 
-// Part 1
+var red = 'rgb(255, 48, 37)';
+var green = 'rgb(42, 233, 69)';
 var trace1 = {
-    x: ["beer", "wine", "martini", "margarita",
-      "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: "bar"
+    x: [1, 1, 1, 1],
+    y: [70000, 125000, 310000, 700000],
+    mode: 'markers',
+    marker: {
+      size: [3, 10, 20, 60],
+      color: [red, green, red, green],
+      opacity: [0.4, 0.4, 0.4, 0.4],
+    }
   };
   
   var data = [trace1];
   
   var layout = {
-    title: "'Bar' Chart"
+    showline: false,
+    title: 'House Price 2014 vs. 2021',
+    showlegend: false,
+    height: 800,
+    width: 800
   };
   
   Plotly.newPlot("plot-garrett", data, layout);

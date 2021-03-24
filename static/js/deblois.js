7411c15233e62a11748e29ae307a13965f7e91d2
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-03-24 15:01:18 -0500

    got the hover text for the bubble chart formatted properly

--- static/js/deblois.js
+++ static/js/deblois.js
@@ -6,21 +6,27 @@ function GetCityName() {
 d3.json('/api/house_data/bubblechart', function (error, responseData) {
   if (error) return console.warn(error);
 
-  console.log(responseData);
-
   var red = 'rgb(255, 48, 37)';
   var green = 'rgb(42, 233, 69)';
   var cityX = Array.from({ length: 250 }, (x, i) => i + 1); //number of cities in the house data set (250 cities will be final number)
+  var hoverCityText = responseData[0].City.map(i => 'City: ' + i);
+  var hoverPriceText = responseData[0].Price.map(i => '<br>Avg House Price 2021: $' + i.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'));
+  var hoverAmountChangeText = responseData[0].MarkerSize.map(i => '<br>Amount Change: $' + (i*1000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'));
+  var finalHoverText = [];
+
+  for (var i = 0; i < hoverCityText.length; i++) {
+    finalHoverText.push(hoverCityText[i] + hoverPriceText[i] + hoverAmountChangeText[i]);
+}
 
   var trace1 = {
     x: cityX,
     y: responseData[0].Price, //house sell price
-    //text: ['City: St. Louis<br>Avg House Price 2021: $70,000<br>Amount Change: $-3,000', 'City: Austin<br>Avg House Price 2021: $125,000<br>Amount Change: $10,000', 'City: San Francisco<br>Avg House Price 2021: $310,000<br>Amount Change: $-20,000', 'City: New York City<br>Avg House Price 2021: $700,000<br>Amount Change: $60,000'],
+    text: finalHoverText,
     mode: 'markers',
     marker: {
       size: responseData[0].MarkerSize, //relative price change for 2014 vs 2020
       color: green,
-      opacity: 0.4 //see if i can set all points to the same opacity
+      opacity: 0.3
     }
   };
 

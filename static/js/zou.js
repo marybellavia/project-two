3072191532126f5d4e1608fd95a96a9a71436721
Irene-zou <69069854+Irene-zou@users.noreply.github.com>
2021-03-23 19:33:20 -0500

    add code for price graph by state over time

---
+++ static/js/zou.js
@@ -0,0 +1,77 @@
+Plotly.d3.json("/api/rent_data", function(err, rows){
+
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+    }
+
+var allStateNames = unpack(rows, 'State'),
+    allYear = unpack(rows, 'Year'),
+    allPrice = unpack(rows, 'Price'),
+    listofStates = [],
+    currentState,
+    currentPrice = [],
+    currentYear = [];
+
+  for (var i = 0; i < allStateNames.length; i++ ){
+    if (listofStates.indexOf(allStateNames[i]) === -1 ){
+        listofStates.push(allStateNames[i]);
+    }
+  }
+  
+  function getStateData(chosenState) {
+    currentPrice = [];
+    currentYear = [];
+    for (var i = 0 ; i < allStateNames.length ; i++){
+      if ( allStateNames[i] === chosenState) {
+        currentPrice.push(allPrice[i]);
+        currentYear.push(allYear[i]);
+      } 
+    }
+  };
+
+// Default State Data
+setBubblePlot('New York');
+  
+function setBubblePlot(chosenState) {
+    getStateData(chosenState);  
+
+    var trace1 = {
+      x: currentYear,
+      y: currentPrice,
+      mode: 'lines+markers',
+      marker: {
+        size: 12, 
+        opacity: 0.5
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: 'Price for rental by State<br>'+ chosenState + ' Price'
+    };
+
+    Plotly.newPlot("plot-irene", data, layout);
+};
+  
+var innerContainer = document.querySelector('[data-num="0"'),
+    plotEl = innerContainer.querySelector('.plot'),
+    stateSelector = innerContainer.querySelector('.statedata');
+
+function assignOptions(textArray, selector) {
+  for (var i = 0; i < textArray.length;  i++) {
+      var currentOption = document.createElement('option');
+      currentOption.text = textArray[i];
+      selector.appendChild(currentOption);
+  }
+}
+
+assignOptions(listofCountries, stateSelector);
+
+function updateState(){
+    setBubblePlot(stateSelector.value);
+}
+  
+stateSelector.addEventListener('change', updateState, false);
+});
+

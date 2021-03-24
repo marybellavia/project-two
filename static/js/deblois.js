b912f0e322097528e0b6150a477e553d4e3d9996
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-03-24 08:08:00 -0500

    adding latest code. trying to get the API response content into the traces for plotly.

--- static/js/deblois.js
+++ static/js/deblois.js
@@ -1,21 +1,27 @@
 function GetCityName(){
     alert(document.getElementById('citySearchBox').value);
     document.getElementById('citySearchBox').value = "";
+    console.log(responseData[0]);
 }
+
+var responseData = '';
+fetch('/api/house_data/bubblechart')
+  .then(response => response.json())
+  .then(data =>  responseData = data);
 
 var red = 'rgb(255, 48, 37)';
 var green = 'rgb(42, 233, 69)';
-var cityX = Array.from({length: 4}, (x, i) => i+1); //number of cities in the house data set (893 cities will be final number)
+var cityX = Array.from({length: 250}, (x, i) => i+1); //number of cities in the house data set (893 cities will be final number)
 
 var trace1 = {
     x: cityX,
-    y: [70000, 125000, 310000, 700000], //house sell price
-    text: ['City: St. Louis<br>Avg House Price 2021: $70,000<br>Amount Change: $-3,000', 'City: Austin<br>Avg House Price 2021: $125,000<br>Amount Change: $10,000', 'City: San Francisco<br>Avg House Price 2021: $310,000<br>Amount Change: $-20,000', 'City: New York City<br>Avg House Price 2021: $700,000<br>Amount Change: $60,000'],
+    y: responseData[0].Price, //house sell price
+    //text: ['City: St. Louis<br>Avg House Price 2021: $70,000<br>Amount Change: $-3,000', 'City: Austin<br>Avg House Price 2021: $125,000<br>Amount Change: $10,000', 'City: San Francisco<br>Avg House Price 2021: $310,000<br>Amount Change: $-20,000', 'City: New York City<br>Avg House Price 2021: $700,000<br>Amount Change: $60,000'],
     mode: 'markers',
     marker: {
-      size: [3, 10, 20, 60], //relative price change for 2014 vs 2020
-      color: [red, green, red, green],
-      opacity: [0.4, 0.4, 0.4, 0.4] //see if i can set all points to the same opacity
+      size: responseData[0].MarkerSize, //relative price change for 2014 vs 2020
+      color: green,
+      opacity: 0.4 //see if i can set all points to the same opacity
     }
   };
   
